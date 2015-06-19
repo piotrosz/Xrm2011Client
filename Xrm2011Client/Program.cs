23b190bff74cf0937b66daa23880d3f7d29441f5
Piotrosz <piotrosz@gmail.com>
2015-06-19 20:27:03 +0200

    Initial commit

---
+++ Xrm2011Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.Xrm.Client;
+using Microsoft.Xrm.Client.Services;
+
+namespace XrmQuery
+{
+    class Program
+    {
+        static void Main()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["Xrm"].ConnectionString;
+            var connection = CrmConnection.Parse(connectionString);
+
+            using (var organizationService = new OrganizationService(connection))
+            {
+                
+            }
+        }
+    }
+}
